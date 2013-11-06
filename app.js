75ce11a8826752644a3733b21e1f3f2b363640fb
Jeffery McLean <jeffery.mclean@gmail.com>
2013-11-06 05:13:02 +0000

    second commit

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 $(function() {
-	$('#submit-button').click( function() {
-		alert('screw off');
-	});  
+
+  	$("ul").on( "click", "li", function() {
+    	$(this).remove();
+    });
+
+	$('#btn-add-item').click( function() {
+		//alert('You added an item!');
+		bla = $('#txt_name').val();
+		$("#list").append("<li>" + bla + "</li>");
+		//$(this).closest('li').slideUp().remove();
+    	$("#txt_name").val("");
+    	                                      
+		
+
+	});
 });
 
+
+
+
+
+
+// $( "body" ).on( "click", "p", function() {
+//   alert( $( this ).text() );
+// });
